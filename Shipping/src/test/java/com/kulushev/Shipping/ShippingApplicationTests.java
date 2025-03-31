ca17e9ec92480710ab34877551773aafbb2e515f
Aleksey Kulushev <alexcoolarbitr@gmail.com>
2025-03-31 16:53:24 +0300

    Init commit

---
+++ Shipping/src/test/java/com/kulushev/Shipping/ShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kulushev.Shipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
